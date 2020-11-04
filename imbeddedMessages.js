e41acb65a9eecf45b8b6a87eb05ccc5345fb7e54
chloecaillaud <chloecaillaud.3d@gmail.com>
2020-11-03 16:05:01 -0800

    added base files

---
+++ imbeddedMessages.js
@@ -0,0 +1,50 @@
+const helpMessage = 
+{
+	color: 0x23272a,
+	title: 'Help',
+	description: '(these options are required)[these are optional]',
+/*	thumbnail: {
+		url: '',
+	},
+*/	fields: [
+		{
+			name: 'cb!clearnick',
+			value: 'clear your nickname',
+			inline: true,
+		},
+		{
+			name: 'cb!nick [@username] (nickname)',
+			value: 'set the nickname of someone or yourself(if not specified)',
+			inline: true,
+		},
+		{
+			name: 'cb!nukechannel [amount]',
+			value: '(admin only) clear messages from channel',
+			inline: true,
+		},
+		{
+			name: 'cb!setup',
+			value: '!work in prgress! doesn\'t currently do anything',
+			inline: true,
+		},
+		{
+			name: 'cb!badbot',
+			value: 'removes the last fredboat message',
+			inline: true,
+		},
+		{
+			name: 'cb!support',
+			value: '!work in prgress! doesn\'t currently do anything',
+			inline: true,
+		},
+	],
+/*	image: {
+		url: 'https://i.imgur.com/wSTFkRM.png',
+	},
+	footer: {
+		text: 'Some footer text here',
+		icon_url: 'https://i.imgur.com/wSTFkRM.png',
+	},*/
+};
+
+module.exports.helpMessage = helpMessage;
